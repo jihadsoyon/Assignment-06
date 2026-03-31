ea6d28fbff5255eb90c3e10f044f0e8a3a0d6c39
jihadsoyon <jihadsoyon@gmail.com>
2026-03-31 18:51:34 +0600

    Card Section dynamic by api, step section designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-
+import { useState } from "react";
 import { ToastContainer } from 'react-toastify'
 import './App.css'
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
-function App() {
+import Stats from './components/Stats';
+import MainSection from "./components/MainSection";
+import Steps from "./components/Steps";
 
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems((prev) => {
+      if (prev.find((item) => item.id === product.id)) return prev;
+      return [...prev, product];
+    });
+  };
+
+  const handleRemoveFromCart = (id) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const handleCheckout = () => {
+    setCartItems([]);
+  };
 
   return (
-<div className="min-h-screen bg-white font-sans">
-    <Navbar  />
-   <Banner />
+    <div className="min-h-screen bg-white font-sans">
+      <Navbar cartCount={cartItems.length} />
+      <Banner />
+      <Stats />
+      <MainSection
+        cartItems={cartItems}
+        onAddToCart={handleAddToCart}
+        onRemove={handleRemoveFromCart}
+        onCheckout={handleCheckout}
+      />
+      <Steps/>
 
-
-
-{/* React Toastify */}
-       <ToastContainer />   
-   </div>
+      {/* React Toastify */}
+      <ToastContainer />
+    </div>
   )
 }
 
