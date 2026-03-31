7915b6e89c4342f3a461e918737fa4373cbee07b
jihadsoyon <jihadsoyon@gmail.com>
2026-03-31 16:30:54 +0600

    set up & install tailwind, daisyui, toastify, lucide react & also generate the json data

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+          <h1>Get started</h1>
+
+
+{/* React Toastify */}
+       <ToastContainer />   
+    </>
+  )
+}
+
+export default App
